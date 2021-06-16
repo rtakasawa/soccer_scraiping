f61050e0a03fdf9adb3f156f0395a954186fa950
ryosuke takasawa <ten.r0619@gmail.com>
2021-06-16 11:58:38 +0900

    wheneverを使ったバッチ処理実行
    -mherokuではwheneverが動かないため本番環境では使用しない

---
+++ config/schedule.rb
@@ -0,0 +1,12 @@
+# 出力先のログファイルを指定
+set :output, 'log/crontab.log'
+# ジョブの実行環境の指定
+set :enviroment, :production
+
+every 1.day, at: '11:40 am' do
+  runner "Nishigaoka.scraiping"
+end
+
+every 1.day, at: '11:55 am' do
+  rake "push_line:this_month_match_info"
+end
